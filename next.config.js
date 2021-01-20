8804afbf0bf9c3aa7bb655bc94c1e8f6f5d4a02d
moh-websolutions <mahmoud@websolutions.ca>
2021-01-20 09:19:13 -0500

    update 1

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-	i18n: {
-	   domains: [
-	      {
-	        domain: 'bimbodaycare.ca',
-	        defaultLocale: 'en-US',
-	      },
-	      {
-	        domain: 'garderiebimbo.ca',
-	        defaultLocale: 'fr',
-	      },
-	    ],
-	},
-   
-};
-
 const nextTranslate = require('next-translate');
 
 module.exports = {
